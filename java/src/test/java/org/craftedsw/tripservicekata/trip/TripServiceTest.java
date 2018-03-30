c70bb9efe7f6ddb20d33a0716c454c40ae06ce09
venaud <34676647+venaud@users.noreply.github.com>
2018-03-30 18:28:59 +0200

    Add a first test for Java

--- java/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,22 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.assertj.core.api.Assertions;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Test;
+
+import java.util.List;
+
 public class TripServiceTest {
-	
+    @Test
+    public void shouldDoSomething() throws Exception {
+        // Given
+        User user = new User();
+        TripService tripService = new TripService();
+
+        // When
+        List<Trip> tripsOfGivenUser = tripService.getTripsByUser(user);
+
+        // Then
+        Assertions.assertThat(tripsOfGivenUser).isEqualTo("Not an exception");
+    }
 }
