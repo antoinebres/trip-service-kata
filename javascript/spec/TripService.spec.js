0617d84e99b13a84496fc70af3678c53d6ae2fe6
venaud <34676647+venaud@users.noreply.github.com>
2018-03-30 18:37:15 +0200

    Add jest for tests in JS

---
+++ javascript/spec/TripService.spec.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const expect = require('chai').expect
+const TripService = require('../src/TripService');
+
+describe('TripService', () => {
+
+    it('should... ', () => {
+        expect(2+2).to.equal(5);
+    });
+
+});
---
+++ javascript/spec/TripService.test.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const TripService = require('../src/TripService');
+
+describe('TripService', () => {
+
+    it('should... ', () => {
+        expect(2+2).toEqual(5);
+    });
+
+});
