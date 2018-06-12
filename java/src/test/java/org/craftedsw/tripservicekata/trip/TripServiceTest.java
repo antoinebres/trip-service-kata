6eaac7b2d27ccf25da35de331d2ce10b2268e1f3
Abel André <aandre@octo.com>
2018-06-12 11:11:12 +0200

    Starter project without mockito and spring

--- java/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,22 +1,13 @@
 package org.craftedsw.tripservicekata.trip;
 
-import org.assertj.core.api.Assertions;
-import org.craftedsw.tripservicekata.user.User;
 import org.junit.Test;
 
-import java.util.List;
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class TripServiceTest {
+
     @Test
-    public void shouldDoSomething() throws Exception {
-        // Given
-        User user = new User();
-        TripService tripService = new TripService();
-
-        // When
-        List<Trip> tripsOfGivenUser = tripService.getTripsByUser(user);
-
-        // Then
-        Assertions.assertThat(tripsOfGivenUser).isEqualTo("Not an exception");
+    public void MyFirstTest() {
+        assertThat(true).isTrue();
     }
 }
