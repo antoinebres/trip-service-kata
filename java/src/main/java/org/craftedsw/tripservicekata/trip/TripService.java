ec9e9590cfa615102597adaf7e320a2f2bf0ca81
Abel André <aandre@octo.com>
2018-06-12 12:04:34 +0200

    coverage done with dirty hack

--- java/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -3,30 +3,39 @@ package org.craftedsw.tripservicekata.trip;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.sun.istack.internal.NotNull;
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 
 public class TripService {
 
-	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = UserSession.getInstance().getLoggedUser();
+	public List<Trip> getTripsByUser(@NotNull User user) throws UserNotLoggedInException {
+		List<Trip> trips = new ArrayList<Trip>();
+		User currentUser = getLoggedUser();
 		boolean isFriend = false;
-		if (loggedUser != null) {
+		if (currentUser != null) {
 			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
+				if (friend.equals(currentUser)) {
 					isFriend = true;
 					break;
 				}
 			}
 			if (isFriend) {
-				tripList = TripDAO.findTripsByUser(user);
-			}
-			return tripList;
+                trips = fetchTripsByUser(user);
+            }
+			return trips;
 		} else {
 			throw new UserNotLoggedInException();
 		}
 	}
-	
+
+    protected List<Trip> fetchTripsByUser(User user) {
+        return TripDAO.findTripsByUser(user);
+    }
+
+    protected User getLoggedUser() {
+		return UserSession.getInstance().getLoggedUser();
+	}
+
 }
--- java/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,13 +1,82 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class TripServiceTest {
 
+    private User currentUser;
+
     @Test
-    public void MyFirstTest() {
-        assertThat(true).isTrue();
+    public void getTripsByUser_return_no_trips_when_logged_user_has_no_friends() {
+        // given
+        TripService tripService = new TestableTripService();
+        User user = new User();
+        currentUser = new User();
+
+        // when
+        List<Trip> tripsByUser = tripService.getTripsByUser(user);
+
+        // then
+        assertThat(tripsByUser).isEmpty();
+    }
+
+    @Test
+    public void getTripsByUser_return_a_trip_when_logged_user_is_friend_with_given_user() {
+        // given
+        TripService tripService = new TestableTripService();
+        User user = new User();
+        currentUser = new User();
+        User otherFriend = new User();
+        user.addFriend(otherFriend);
+        user.addFriend(currentUser);
+
+        // when
+        List<Trip> tripsByUser = tripService.getTripsByUser(user);
+
+        // then
+        assertThat(tripsByUser).isNotEmpty();
+    }
+
+    @Test(expected = UserNotLoggedInException.class)
+    public void getTripsByUser_throws_an_UserNotLoggedInException_when_user_is_not_logged_in() {
+        // given
+        TripService tripService = new TestableTripService();
+        User user = new User();
+        currentUser = null;
+
+        // when
+        tripService.getTripsByUser(user);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void getTripsByUser_throws_a_null_pointer_exception_when_given_user_os_null() {
+        // given
+        TripService tripService = new TestableTripService();
+        currentUser = new User();
+
+        // when
+        tripService.getTripsByUser(null);
+    }
+
+    class TestableTripService extends TripService {
+
+        @Override
+        protected User getLoggedUser() {
+            return currentUser;
+        }
+
+        @Override
+        protected List<Trip> fetchTripsByUser(User user) {
+            List<Trip> trips = new ArrayList<Trip>();
+            trips.add(new Trip());
+            return trips;
+        }
     }
 }
