b9104446d6b7d270e6c23c8b561d42a77726f6bd
Antoine Brès <octo-anbr@MacBook-Pro-3.paris.octo>
2018-07-13 12:23:36 +0200

    coverage done

--- java/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,22 +1,84 @@
 package org.craftedsw.tripservicekata.trip;
 
 import org.assertj.core.api.Assertions;
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.List;
+import java.util.*;
 
 public class TripServiceTest {
-    @Test
-    public void shouldDoSomething() throws Exception {
+    private static final Trip TO_BRAZIL = new Trip();
+    private static final Trip TO_LONDON = new Trip();
+    private static final User ANOTHER_USER = new User();
+    private User loggedUser;
+    private User GUEST = null;
+    private User REGISTERED_USER = new User();
+    private TripService tripService;
+
+    @Before
+    public void initialise() {
+        tripService = new TestableTripService();
+        loggedUser = REGISTERED_USER;
+    }
+
+    @Test(expected = UserNotLoggedInException.class)
+    public void shouldTrowAnExceptionWhenUserNotLoggedIn() throws Exception {
         // Given
+        loggedUser = GUEST;
         User user = new User();
-        TripService tripService = new TripService();
+        TripService tripService = new TestableTripService();
 
         // When
-        List<Trip> tripsOfGivenUser = tripService.getTripsByUser(user);
+        tripService.getTripsByUser(user);
 
         // Then
-        Assertions.assertThat(tripsOfGivenUser).isEqualTo("Not an exception");
+
     }
+
+    @Test
+    public void shouldReturnNoTripsWhenUserAreNotFriends() throws Exception {
+        // Given
+        User notAFriend = new User();
+        notAFriend.addTrip(TO_BRAZIL);
+        TripService tripService = new TestableTripService();
+
+        // When
+        List<Trip> friendTrips = tripService.getTripsByUser(notAFriend);
+
+        // Then
+        Assertions.assertThat(friendTrips.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void shouldReturnTripsWhenUserAreFriends() throws Exception {
+        // Given
+        User aFriend = new User();
+        aFriend.addFriend(ANOTHER_USER);
+        aFriend.addFriend(loggedUser);
+        aFriend.addTrip(TO_BRAZIL);
+        aFriend.addTrip(TO_LONDON);
+        TripService tripService = new TestableTripService();
+
+
+        // When
+        List<Trip> friendTrips = tripService.getTripsByUser(aFriend);
+
+        // Then
+        Assertions.assertThat(friendTrips.size()).isEqualTo(2);
+    }
+
+
+    public class TestableTripService extends TripService{
+        @Override
+        protected User getLoggedUser() {
+            return loggedUser;
+        }
+        @Override
+        protected List<Trip> tripsBy(User user) {
+            return user.trips();
+        }
+    }
+
 }
